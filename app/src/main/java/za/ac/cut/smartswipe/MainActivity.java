0c5481e0e1eb22e47ee77b1e08e27a089cd5981c
hodiel <siyabonga.nano@gmail.com>
2016-05-07 16:25:07 +0200

    initial commit

---
+++ app/src/main/java/za/ac/cut/smartswipe/MainActivity.java
@@ -0,0 +1,20 @@
+package za.ac.cut.smartswipe;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/*Created by: S. Nano - 215110048
+* Name:Smart Swipe
+* Purpose: Simulates the operation of a teller PIN
+* (Personal Identification Number) pad.
+* */
+
+public class MainActivity extends Activity {
+
+    /* Called when the activity is first created. */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
